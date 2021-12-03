0062cc869f709cde21bb92f1f0f5c51429c775dc
k1105 <kanapon1105@yahoo.co.jp>
2021-12-03 10:08:41 +0900

    Echolocationの発生源をインスペクタ上で指定できるよう修正

--- Scripts/EcholocationTrigger.cs
+++ Scripts/EcholocationTrigger.cs
@@ -1,30 +1,32 @@
 using UnityEngine;
 
+/// description ///
+/// エコロケーションの生成を行うスクリプト.
+/// デフォルトでは, マウスパッドがクリックされた時にエコロケーションの生成を行う.
+/// またその時の中心位置は, インスペクタ上で指定されたゲームオブジェクトのpositionを基準にする.
+
 [RequireComponent(typeof(MultiEcholocationController))]
 public class EcholocationTrigger : MonoBehaviour
 {
+    public GameObject Origin = null;
     private MultiEcholocationController controller;
-    private Camera mainCamera;
     private AudioSource aud;
 
     private void Start()
     {
-        this.mainCamera = Camera.main;
         this.controller = this.GetComponent<MultiEcholocationController>();
     }
 
     private void Update()
     {
-
-        // if (Input.GetMouseButtonDown(0) && Physics.Raycast(this.mainCamera.ScreenPointToRay(Input.mousePosition), out var hitInfo))
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) // 入力方法を変更したい場合はここの一行を修正する. default: クリックでEcholocationを生成.
         {
-            if (this.mainCamera != null) {
-                this.controller.EmitCall(this.mainCamera.transform.position);
+            if (Origin != null) {
+                this.controller.EmitCall(Origin.transform.position);
             // this.controller.EmitCall(hitInfo.point);
             } else {
-                Debug.Log("Error: no camera exsist.");
-                Debug.Log("上記のエラーはCamera.mainとして使用したいカメラのtagをMainCameraに設定すると解決(21/12/02)");
+                Debug.Log("Error: no object exsist.");
+                Debug.Log("インスペクタ上でCenterにエコロケーションシェーダーの発生源になるゲームオブジェクトを指定してください. ");
             }
             
         }
